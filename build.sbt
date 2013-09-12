33c0a7f0e83c1f842b96a3646fc3514afaeeedec
Jason Pearson <jason.d.pearson@gmail.com>
2013-09-12 18:37:12 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "algorithms-assignment-1"
+
+version := "1.0"
+
+scalaVersion := "2.10.2"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+  "junit" % "junit" % "4.11" % "test",
+  "com.novocode" % "junit-interface" % "0.7" % "test->default"
+)
+
+testOptions += Tests.Argument(TestFrameworks.JUnit, "-v")
+s
---
+++ src/main/java/Percolation.java
@@ -0,0 +1,25 @@
+
+public class Percolation {
+
+    private int n;
+
+    public Percolation(int n) {
+        this.n = n;
+    }
+
+    public void open(int i, int j) {
+
+    }
+
+    public boolean isOpen(int i, int j) {
+        return false;
+    }
+
+    public boolean isFull(int i, int j) {
+        return false;
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+}
